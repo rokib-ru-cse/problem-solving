51891c8e4553c10352ddbed0c1db1e0e675071d9
Rokib Ahmed <rokibahmed.ru.cse@gmail.com>
2023-03-19 08:59:28 +0000

    .

--- FindTheCommonElementsInArrays.java
+++ FindTheCommonElementsInArrays.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * FindTheCommonElementsInArrays
@@ -31,34 +33,35 @@ public class FindTheCommonElementsInArrays {
         Arrays.sort(intRes);
         return intRes;
     }
-    public int[] findTheCommonElementsInArraysByLooping(int[] arr1,int[] arr2){
-        int arr1Max = Arrays.stream(arr1).max().getAsInt();
-        int arr2Max = Arrays.stream(arr2).max().getAsInt();
-        int[] temparr1 = new int[arr1Max+1];
-        int[] temparr2 = new int[arr2Max+1];
-        for (int i : arr1) {
-            temparr1[i]++;
-        }
-        for (int i : arr2) {
-            temparr2[i]++;
-        }
+    public int[] findTheCommonElementsInArraysByLinkHashMap(int[] arr1,int[] arr2){
+        
+        Map<Integer,Integer> map = new LinkedHashMap<>();
         List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < temparr1.length; i++) {
-            if(temparr1[i]>0 && temparr2[i]>0){
-                int min = Math.min(temparr1[i],temparr2[i]);
-                for (int j = 0; j < min; j++) {
-                    res.add(i);
-                }
+        for (int i : arr1) {
+            if(map.containsKey(i)){
+                map.put(i,map.get(i)+1);
+            }else{
+                map.put(i,1);
             }
         }
+        for (int i : arr2) {
+            if(map.containsKey(i)){
+                res.add(i);
+                map.put(i,map.get(i)-1);
+            }
+        }
+
         int[] intRes = res.stream().mapToInt(c->c).toArray();
         Arrays.sort(intRes);
         return intRes;
     }
     public static void main(String[] args){
         for (int a : new FindTheCommonElementsInArrays().findTheCommonElementsInArrays(new int[]{1, 2, 3,3, 5, 6, 7}, new int[]{3,3, 6, 7, 8, 20})) {
-        System.out.print(a+" ");
-            
+            System.out.print(a+" ");
+        }
+        System.out.println();
+        for (int a : new FindTheCommonElementsInArrays().findTheCommonElementsInArraysByLinkHashMap(new int[]{1, 2, 3,3, 5, 6, 7}, new int[]{3,3, 6, 7, 8, 20})) {
+            System.out.print(a+" ");
         }
         System.out.println();
     }

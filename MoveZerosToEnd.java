f1bbbfda614e42c818d3ede85bf6e2cc3f95d93a
Rokib Ahmed <rokibahmed.ru.cse@gmail.com>
2023-03-23 07:24:17 +0000

    .

--- MoveZerosToEnd.java
+++ MoveZerosToEnd.java
@@ -13,12 +13,29 @@ public class MoveZerosToEnd {
         for(int i=count;i<arr.length;i++){
             arr[i] = 0;
         }
-
         return  arr;
+    }
+
+    public static int[] moveZerosToEndWithExtraSpace(int[] arr){
+        int count = 0;
+        int[] res = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                res[count++] = arr[i];
+            }
+        }
+        for(int i=count;i<arr.length;i++){
+            res[i] = 0;
+        }
+        return  res;
     }
 
     public static void main(String[] args){
         for(int a:moveZerosToEndWithoutExtraSpace(new int[]{1,2,0,3,0,0,6,9})){
+            System.out.print(a+" ");
+        }
+        System.out.println();
+        for(int a:moveZerosToEndWithExtraSpace(new int[]{1,2,0,3,0,0,6,9})){
             System.out.print(a+" ");
         }
     }

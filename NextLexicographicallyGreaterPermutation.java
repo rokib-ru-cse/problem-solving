8aa3a821bb132369ca8dc99299f3c0502e9238f2
Rokib Ahmed <rokibahmed.ru.cse@gmail.com>
2023-03-27 15:34:35 +0000

    .

---
+++ NextLexicographicallyGreaterPermutation.java
@@ -0,0 +1,10 @@
+public class NextLexicographicallyGreaterPermutation {
+    
+    static int[] nextLexicographicallyGreaterPermutation(int[] arr){
+
+        return null;
+    }
+    public static void main(String[] args) {
+        System.out.println(nextLexicographicallyGreaterPermutation(new int[]{1,2,3}).toString());
+    }
+}

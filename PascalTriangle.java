fef4c61c0490899dd7a955495021c373ae56195a
Rokib Ahmed <rokibahmed.ru.cse@gmail.com>
2023-03-27 15:21:25 +0000

    .

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -7,14 +7,28 @@ class PascalTriangle {
     if(num==1){
         return new int[][]{{1}};
     }
-    return new int[][]{{0}};
+    int[][] res = new int[num][];
+    res[0] = new int[]{1};
+    for(int i=1;i<num;i++){
+      int[] temp = new int[i+1];
+      for(int j=0;j<=i;j++){
+        if(j==0||j==i){
+          temp[j] = 1;
+        }else{
+          temp[j] = res[i-1][j-1]+res[i-1][j];
+        }
+      }
+      res[i] = temp;
+    }
+    return res;
   }
 
   public static void main(String[] args) {
-    for(int[] a:pascalTringle(1)){
+    for(int[] a:pascalTringle(5)){
         for(int b:a){
-            System.out.println(b+" ");
+            System.out.print(b+" ");
         }
+        System.out.println();
     }
   }
 

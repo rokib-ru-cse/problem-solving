72cea7ca7ee595eac289070849436630c05a81ba
Rokib Ahmed <rokibahmed.ru.cse@gmail.com>
2023-03-28 06:25:18 +0000

    .

--- MaximumSubarraySum.java
+++ MaximumSubarraySum.java
@@ -8,25 +8,34 @@ public class MaximumSubarraySum {
         /*
          [-2,1,-3,4,-1,2,1,-5,4] 
          return 6
-        
+        -3+4 = 1
+        -1-3 = -4
         */
-        int previousSum=0;
-        int postSum=0;
+        int previousSum=-1111111;
+        int postSum=arr[0];
         int lastIndex = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                for (int k=lastIndex; k < j; k++) {
-                    post+=arr[k];
+        while (lastIndex<arr.length) {
+            int k=lastIndex;
+            while(true ){
+                if(k<arr.length && postSum+arr[k]>=postSum){
+                    postSum = postSum+arr[k];
+                    k++;
+                }else{
+                    if(postSum>=previousSum){
+                        previousSum=postSum;
+                    }
+                    postSum = 0;
+                    break;
                 }
-                
             }
+            lastIndex++;
+            
         }
-
-        return 0;
+        return previousSum;
     }
 
 
     public static void main(String[] args) {
-        
+        System.out.println(maximumSubarraySum(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
     }
 }

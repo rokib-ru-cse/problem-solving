53fc4e47f40c93b60c13d135c8ead185099d0cf0
Rokib Ahmed <rokib.ahmed@bizzntek.com>
2023-04-02 12:04:31 +0600

    .

--- linkedList/RemoveDuplicateElementFromSortedLinkedList.java
+++ linkedList/RemoveDuplicateElementFromSortedLinkedList.java
@@ -2,11 +2,26 @@ package linkedList;
 
 public class RemoveDuplicateElementFromSortedLinkedList {
 
-static Node removeDuplicateElementFromSortedLinkedList(Node node){
-    Node newRoot = node;
+    static Node removeDuplicateElementFromSortedLinkedList(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Node newRoot = node;
 
-    return  null;
-}
+        Node pre = node;
+        node = node.next;
+        while (node != null) {
+            if (node.value == pre.value) {
+                pre.next = node.next;
+            } else {
+                pre = node;
+            }
+            node = node.next;
+        }
+
+        return newRoot;
+    }
+
     public static void main(String[] args) {
         Node one = new Node(1);
         Node two = new Node(2);
@@ -14,17 +29,19 @@ static Node removeDuplicateElementFromSortedLinkedList(Node node){
         Node three1 = new Node(3);
         Node four = new Node(4);
         Node five = new Node(5);
+        Node five1 = new Node(5);
 
         one.next = two;
         two.next = three;
         three.next = three1;
         three1.next = four;
         four.next = five;
+        five.next = five1;
 
-        Node n = removeDuplicateElementFromSortedLinkedList(one) ;
-        while (n!=null){
-            System.out.print(n.value+"->");
-            n=n.next;
+        Node n = removeDuplicateElementFromSortedLinkedList(one);
+        while (n != null) {
+            System.out.print(n.value + "->");
+            n = n.next;
         }
     }
 }

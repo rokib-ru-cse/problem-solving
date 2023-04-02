bfd891b0e9fcb22323a8fecd601f640956a1f341
Rokib Ahmed <rokib.ahmed@bizzntek.com>
2023-04-02 09:00:39 +0600

    .

--- linkedList/RemoveDuplicateElementFromSortedLinkedList.java
+++ linkedList/RemoveDuplicateElementFromSortedLinkedList.java
@@ -3,18 +3,22 @@ package linkedList;
 public class RemoveDuplicateElementFromSortedLinkedList {
 
 static Node removeDuplicateElementFromSortedLinkedList(Node node){
+    Node newRoot = node;
+
     return  null;
 }
     public static void main(String[] args) {
         Node one = new Node(1);
         Node two = new Node(2);
         Node three = new Node(3);
+        Node three1 = new Node(3);
         Node four = new Node(4);
         Node five = new Node(5);
 
         one.next = two;
         two.next = three;
-        three.next = four;
+        three.next = three1;
+        three1.next = four;
         four.next = five;
 
         Node n = removeDuplicateElementFromSortedLinkedList(one) ;

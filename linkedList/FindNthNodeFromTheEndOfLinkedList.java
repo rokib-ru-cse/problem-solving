ead1f3ffb4a715fc44bda526046193cf130d6194
rokibrucse <rokibahmed.ru.cse@gmail.com>
2023-04-04 22:20:24 +0600

    /

--- linkedList/FindNthNodeFromTheEndOfLinkedList.java
+++ linkedList/FindNthNodeFromTheEndOfLinkedList.java
@@ -2,38 +2,6 @@ package linkedList;
 
 public class FindNthNodeFromTheEndOfLinkedList {
     
-    static Node findNthNodeFromTheEndOfLinkedList(Node root){
-
-
-        return root;
-    }
-
-    public static void main(String[] args) {
-        Node one = new Node(1);
-        Node two = new Node(2);
-        Node three = new Node(4);
-        Node four = new Node(6);
-
-        one.next = two;
-        two.next = three;
-        three.next = four;
-        four.next = two;
-        Node n = findNthNodeFromTheEndOfLinkedList(one);
-        while (n != null) {
-            if (n.next == null) {
-                System.out.print(n.value);
-            } else {
-                System.out.print(n.value + "->");
-            }
-            n = n.next;
-        }
-        System.out.println();
-    }
-}
-package linkedList;
-
-public class FindNthNodeFromTheEndOfLinkedList {
-    
 static Node findNthNodeFromTheEndOfLinkedList(Node root, int nth){
     int total=0;
     Node temp = root;

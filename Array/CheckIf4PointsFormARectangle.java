e2c61e23afc07c33b25b8be932d5690cf1403e4d
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-09 04:37:29 +0600

    /

---
+++ Array/CheckIf4PointsFormARectangle.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class CheckIf4PointsFormARectangle {
+    
+    public static boolean checkIf4PointsFormARectangle(int[][] arr){
+
+
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(checkIf4PointsFormARectangle(new int[][]{{0,0},{2,0},{2,3},{0,3}}));
+    }
+}

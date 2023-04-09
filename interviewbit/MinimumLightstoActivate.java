485044393e94664a08bd537940c53df6aa78711e
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-09 17:29:10 +0600

    .

--- interviewbit/MinimumLightstoActivate.java
+++ interviewbit/MinimumLightstoActivate.java
@@ -7,25 +7,32 @@ import java.util.Arrays;
 public class MinimumLightstoActivate {
     public static int solve(ArrayList<Integer> A, int B) {
         int lastIndex=0;
+        int firstIndex=0;
         int totalLight=0;
+        boolean found = false;
         for (int i = 0; i < A.size(); i++) {
-            if(A.get(i)==1){
-                lastIndex++;
-                if(lastIndex-(B+1)>0){
-                    return -1;
-                }else{
+            firstIndex = i;
+            lastIndex = i+(B-1);
+            if(lastIndex>=A.size()){
+                return  totalLight==0?1:totalLight;
+            }
+            found = false;
+            for (int j = lastIndex; j >=firstIndex; j--) {
+                if(A.get(j)==1){
+                    found = true;
+                    i = j+(B-1);
                     totalLight++;
-                    lastIndex = i;
-                    i = i+(B-1);
+                    break;
                 }
-            }else{
-                lastIndex++;
+            }
+            if(!found){
+                return  -1;
             }
         }
-        return  totalLight;
+        return  totalLight==0?1:totalLight;
     }
     public static void main(String[] args) {
-        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(0, 0, 1, 1, 1, 0, 0, 1));
-        System.out.println(solve(A,3));
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList( 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0));
+        System.out.println(solve(A,12));
     }
 }

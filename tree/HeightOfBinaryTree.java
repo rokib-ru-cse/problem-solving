82adc93e1c5c0c8b9ae299c35e4a54bb62a2557b
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-09 14:58:32 +0600

    .

--- tree/HeightOfBinaryTree.java
+++ tree/HeightOfBinaryTree.java
@@ -1,33 +1,75 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HeightOfBinaryTree {
 
 
-    public static int maxDepth(TreeNode root){
-        if(root==null){
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
             return 0;
         }
         int leftMax = maxDepth(root.leftNode);
         int rightMax = maxDepth(root.rightNode);
-        if(leftMax>rightMax){
-            return leftMax+1;
-        }else{
-            return rightMax+1;
+        if (leftMax > rightMax) {
+            return leftMax + 1;
+        } else {
+            return rightMax + 1;
         }
     }
 
-    public static int maxDepthLoop(TreeNode root){
-        if(root==null){
-            return 0;
+    public static int maxDepthLoop1(TreeNode root) {
+        int depth = 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            TreeNode treeNode = q.peek();
+            q.remove();
+            if (treeNode == null) {
+                depth++;
+            }
+            if (treeNode != null) {
+                if (treeNode.leftNode != null) {
+                    q.add(treeNode.leftNode);
+                }
+                if (treeNode.rightNode != null) {
+                    q.add(treeNode.rightNode);
+                }
+            } else if (!q.isEmpty()) {
+                q.add(null);
+            }
         }
-        int leftMax = maxDepth(root.leftNode);
-        int rightMax = maxDepth(root.rightNode);
-        if(leftMax>rightMax){
-            return leftMax+1;
-        }else{
-            return rightMax+1;
-        }
+        return depth;
     }
+
+    public static int maxDepthLoop2(TreeNode root) {
+        int depth = 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = q.poll();
+                if (treeNode != null) {
+                    if (treeNode.leftNode != null) {
+                        q.add(treeNode.leftNode);
+                    }
+                    if (treeNode.rightNode != null) {
+                        q.add(treeNode.rightNode);
+                    }
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(25);
         TreeNode firstNode = new TreeNode(20);
@@ -68,5 +110,7 @@ public class HeightOfBinaryTree {
         seventhNode.leftNode = twelvethNode;
 
         System.out.println(maxDepth(root));
+        System.out.println(maxDepthLoop1(root));
+        System.out.println(maxDepthLoop2(root));
     }
 }

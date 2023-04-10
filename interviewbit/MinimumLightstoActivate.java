680c1645fef7a23e4cabc54b9ac6a7b053e0e92c
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-10 10:16:47 +0600

    .

--- interviewbit/MinimumLightstoActivate.java
+++ interviewbit/MinimumLightstoActivate.java
@@ -6,33 +6,38 @@ import java.util.Arrays;
 
 public class MinimumLightstoActivate {
     public static int solve(ArrayList<Integer> A, int B) {
-        int lastIndex=0;
-        int firstIndex=0;
-        int totalLight=0;
+        int lastIndex = 0;
+        int firstIndex = 0;
+        int totalLight = 0;
         boolean found = false;
         for (int i = 0; i < A.size(); i++) {
-            firstIndex = i;
-            lastIndex = i+(B-1);
+            //firstIndex = i;
+            lastIndex = i + (B - 1);
+            if (firstIndex >= A.size()) {
+                return totalLight == 0 ? 1 : totalLight;
+            }
             if(lastIndex>=A.size()){
-                return  totalLight==0?1:totalLight;
+                lastIndex=A.size()-1;
             }
             found = false;
-            for (int j = lastIndex; j >=firstIndex; j--) {
-                if(A.get(j)==1){
+            for (int j = lastIndex; j >= firstIndex; j--) {
+                if (A.get(j) == 1) {
                     found = true;
-                    i = j+(B-1);
+                    i = j + (B - 1);
+                    firstIndex = j+1;
                     totalLight++;
                     break;
                 }
             }
-            if(!found){
-                return  -1;
+            if (!found) {
+                return -1;
             }
         }
-        return  totalLight==0?1:totalLight;
+        return totalLight == 0 ? 1 : totalLight;
     }
+
     public static void main(String[] args) {
-        ArrayList<Integer> A = new ArrayList<>(Arrays.asList( 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0));
-        System.out.println(solve(A,12));
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0));
+        System.out.println(solve(A, 12));
     }
 }

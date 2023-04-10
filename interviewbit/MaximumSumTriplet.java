d5eb20eb1f4f3a57f40a1f91cda5dbb68b1872cf
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-10 11:24:33 +0600

    .

--- interviewbit/MaximumSumTriplet.java
+++ interviewbit/MaximumSumTriplet.java
@@ -1,10 +1,40 @@
 package interviewbit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MaximumSumTriplet {
 
+    public static int solve(ArrayList<Integer> A) {
+        int firstPointer = 0;
+        int lastPointer = A.size() - 1;
+        int midPointer = firstPointer + 1;
+        int max = Integer.MIN_VALUE;
+        boolean shouldLastPointerDecrease = true;
+        while (firstPointer < lastPointer-1 && midPointer < lastPointer) {
+            max = Math.max(max, A.get(firstPointer) + A.get(midPointer) + A.get(lastPointer));
+            midPointer++;
+            if (midPointer == lastPointer && shouldLastPointerDecrease) {
+                lastPointer--;
+                midPointer = firstPointer + 1;
+            }else if(midPointer == lastPointer && !shouldLastPointerDecrease) {
+                firstPointer++;
+                midPointer = firstPointer + 1;
+            }
 
+            if(lastPointer-1==firstPointer && shouldLastPointerDecrease){
+                shouldLastPointerDecrease=false;
+                firstPointer = firstPointer+1;
+                lastPointer =A.size()-1;
+                midPointer = firstPointer+1;
+            }
+        }
+        return max;
+    }
 
+    //ans = 88252
     public static void main(String[] args) {
-
+//        System.out.println(solve(new ArrayList<>(Arrays.asList(18468, 6335, 26501, 19170, 15725, 11479, 29359, 26963, 24465, 5706, 28146, 23282, 16828, 9962, 492, 2996, 11943, 4828, 5437, 32392, 14605))));
+        System.out.println(solve(new ArrayList<>(Arrays.asList(2, 5, 3, 1, 4, 9))));
     }
 }

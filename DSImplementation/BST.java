b944ed4fe71f31a82b0d436a19781fa442ae6a97
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-12 07:23:52 +0600

    .

--- DSImplementation/BST.java
+++ DSImplementation/BST.java
@@ -1,16 +1,66 @@
 package DSImplementation;
 
+import com.sun.source.tree.Tree;
+
 /**
  * BST
  */
 public class BST {
 
-    public void insert(TreeNode root,int data){
+    TreeNode root;
 
+    public BST() {
+        this.root = null;
     }
 
+    public BST(int data) {
+        root = new TreeNode(data);
+    }
+
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    public TreeNode insertRec(TreeNode root, int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return root;
+        }
+
+        if (data <= root.data) {
+            root.left = insertRec(root.left, data);
+        } else {
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+    private static void inorderPrint(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        inorderPrint(root.left);
+        System.out.print(root.data+"->");
+        inorderPrint(root.right);
+
+    }
     public static void main(String[] args) {
-            BST root = new BST();
-            
+        /*
+        10
+        /
+       8
+       /\
+      7  9
+         */
+
+        BST tree = new BST();
+        tree.insert(10);
+        tree.insert(8);
+        tree.insert(7);
+        tree.insert(9);
+        tree.insert(12);
+        inorderPrint(tree.root);
+        System.out.println();
     }
+
+
 }

410d68733081384eccb24cde9e8319cee58f3f5f
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-16 15:49:26 +0600

    .

--- DataStructuresAndAlgorithmsInJava/SinglyLinkedList_35.java
+++ DataStructuresAndAlgorithmsInJava/SinglyLinkedList_35.java
@@ -2,7 +2,44 @@ package DataStructuresAndAlgorithmsInJava;
 
 public class SinglyLinkedList_35 {
 
-    public static void main(String[] args) {
+    SinglyLinkNode head;
 
+
+    public static void main(String[] args) {
+        SinglyLinkedList_35 head = new SinglyLinkedList_35();
+        head.add(10);
+        head.add(5);
+        head.add(15);
+        head.add(20);
+        head.add(0);
+        head.add(100);
+        head.add(200);
+        head.add(300);
+        head.print();
     }
+
+    private void print() {
+        SinglyLinkNode temp = head;
+        while (temp != null) {
+            if (temp.next == null) {
+                System.out.print(temp.data);
+            } else {
+                System.out.print(temp.data + "->");
+            }
+            temp = temp.next;
+        }
+    }
+
+    private void add(int data) {
+        if (head == null) {
+            head = new SinglyLinkNode(data);
+            return;
+        }
+        SinglyLinkNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = new SinglyLinkNode(data);
+    }
+
 }

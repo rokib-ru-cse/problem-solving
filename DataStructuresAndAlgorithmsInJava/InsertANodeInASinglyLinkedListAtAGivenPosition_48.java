41ce18aabbfce8b1c55cd0d147b594898c7a98b6
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-04-17 17:31:01 +0600

    .

--- DataStructuresAndAlgorithmsInJava/InsertANodeInASinglyLinkedListAtAGivenPosition_48.java
+++ DataStructuresAndAlgorithmsInJava/InsertANodeInASinglyLinkedListAtAGivenPosition_48.java
@@ -1,7 +1,7 @@
 package DataStructuresAndAlgorithmsInJava;
 
 public class InsertANodeInASinglyLinkedListAtAGivenPosition_48 {
-SinglyLinkNode head;
+    SinglyLinkNode head;
 
     public static void main(String[] args) {
         InsertANodeInASinglyLinkedListAtAGivenPosition_48 head = new InsertANodeInASinglyLinkedListAtAGivenPosition_48();
@@ -11,22 +11,47 @@ SinglyLinkNode head;
         head.add(5);
         head.add(100);
         head.print();
+        head.add(3,50);
+        head.print();
 
+
+    }
+
+    private void add(int position, int data) {
+        SinglyLinkNode temp = head;
+        int counter = 1;
+        while (temp!=null&&counter!=position){
+            temp = temp.next;
+            counter++;
+        }
+        SinglyLinkNode newNode = new SinglyLinkNode(data);
+        SinglyLinkNode nextAddress = temp.next;
+        temp.next = newNode;
+        newNode.next = nextAddress;
     }
 
     private void print() {
-
+        SinglyLinkNode temp = head;
+        while (temp != null) {
+            if (temp.next == null) {
+                System.out.print(temp.data);
+            } else {
+                System.out.print(temp.data + "->");
+            }
+            temp = temp.next;
+        }
+        System.out.println();
     }
 
     private void add(int data) {
-        if(head==null){
+        if (head == null) {
             head = new SinglyLinkNode(data);
             return;
         }
         SinglyLinkNode temp = head;
-        while (temp.getNext()!=null){
-                temp = temp.getNext();
+        while (temp.getNext() != null) {
+            temp = temp.getNext();
         }
-        temp = new SinglyLinkNode(data);
+        temp.setNext(new SinglyLinkNode(data));
     }
 }

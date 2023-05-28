8701c72ee61f44da5010dd3d418ec256a7f17184
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-05-28 22:20:35 +0600

    ..

---
+++ leetcode/RangeSumQueryImmutable_303.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class RangeSumQueryImmutable_303 {
+
+    public RangeSumQueryImmutable_303(int[] nums) {
+
+    }
+
+    public int sumRange(int left, int right) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

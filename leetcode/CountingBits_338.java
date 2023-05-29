f40d75631722fac29e74dca52b1d651a033b0a9a
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-05-29 21:46:39 +0600

    s

---
+++ leetcode/CountingBits_338.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class CountingBits_338 {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
--- leetcode/RangeSumQueryImmutable_303.java
+++ leetcode/RangeSumQueryImmutable_303.java
@@ -2,12 +2,20 @@ package leetcode;
 
 public class RangeSumQueryImmutable_303 {
 
-    public RangeSumQueryImmutable_303(int[] nums) {
+    int[] nums;
 
+    public RangeSumQueryImmutable_303(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        this.nums = nums;
     }
 
     public int sumRange(int left, int right) {
-        return 0;
+        if (left == 0) {
+            return nums[right];
+        }
+        return nums[right] - nums[left];
     }
 
     public static void main(String[] args) {

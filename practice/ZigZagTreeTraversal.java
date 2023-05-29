e415f5e49ee29c2e75503b23151c0080954a1016
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2023-05-30 00:19:53 +0600

    .

---
+++ practice/ZigZagTreeTraversal.java
@@ -0,0 +1,104 @@
+package practice;
+
+
+import tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ZigZagTreeTraversal {
+
+    TreeNode root;
+
+
+    public static void main(String[] args) {
+        ZigZagTreeTraversal tree = new ZigZagTreeTraversal();
+        tree.root = tree.insert(10);
+        tree.root = tree.insert(5);
+        tree.root = tree.insert(12);
+        tree.root = tree.insert(1);
+        tree.root = tree.insert(6);
+        tree.root = tree.insert(11);
+        tree.root = tree.insert(20);
+
+        tree.print(tree.root);
+        System.out.println();
+        tree.printZigZag(tree.root);
+    }
+
+    private void printZigZag(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> currentLevel = new Stack<>();
+        Stack<TreeNode> nextLevel = new Stack<>();
+        boolean isLeftToRight = true;
+        currentLevel.add(root);
+        while (!currentLevel.isEmpty()) {
+            TreeNode temp = currentLevel.pop();
+            System.out.print(temp.data + " ");
+            if (isLeftToRight) {
+                if (temp.left != null) {
+                    nextLevel.add(temp.left);
+                }
+                if (temp.right != null) {
+                    nextLevel.add(temp.right);
+                }
+            } else {
+                if (temp.right != null) {
+                    nextLevel.add(temp.right);
+                }
+                if (temp.left != null) {
+                    nextLevel.add(temp.left);
+                }
+            }
+            if (currentLevel.isEmpty()) {
+                currentLevel = nextLevel;
+                nextLevel = new Stack<>();
+                isLeftToRight = !isLeftToRight;
+            }
+        }
+    }
+
+    private void print(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        print(root.left);
+        System.out.print(root.data + " ");
+        print(root.right);
+    }
+
+    private TreeNode insert(int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return root;
+        }
+
+        TreeNode lastInsertedNodeParent = lastInsertedNodeParent(root);
+        if (lastInsertedNodeParent.left == null) {
+            lastInsertedNodeParent.left = new TreeNode(data);
+        } else {
+            lastInsertedNodeParent.right = new TreeNode(data);
+        }
+        return root;
+    }
+
+    private TreeNode lastInsertedNodeParent(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            if (temp.left != null && temp.right != null) {
+                queue.add(temp.left);
+                queue.add(temp.right);
+            } else {
+                return temp;
+            }
+        }
+        return null;
+    }
+
+
+}
--- tree/ZigZagTreeTraversal.java
+++ tree/ZigZagTreeTraversal.java
@@ -2,30 +2,40 @@ package tree;
 
 import tree.TreeNode;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class ZigZagTreeTraversal {
+
     TreeNode root;
-    int size = 0;
 
     public static void main(String[] args) {
         ZigZagTreeTraversal tree = new ZigZagTreeTraversal();
-        tree.insert(100);
-        tree.insert(2);
-        tree.insert(10);
-        tree.insert(110);
-        tree.insert(11);
-        tree.insert(12);
-        tree.insert(13);
-        tree.insert(14);
-        tree.insert(15);
-        tree.insert(16);
-        //tree.print(tree.root);
-        System.out.println();
-        tree.printZigZag(tree.root);
+        tree.root = new TreeNode(10);
+
+        tree.root.left = new TreeNode(5);
+        tree.root.right = new TreeNode(15);
+
+        tree.root.left.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(6);
+
+        tree.root.right.left = new TreeNode(11);
+        tree.root.right.right = new TreeNode(20);
+        tree.print(tree.root);
+
+        /*
+         *           10
+         *          /   \
+         *         5     15
+         *       /  \    /  \
+         *      4    6  11   20
+         *
+         *
+         * */
     }
 
-    private void printZigZag(TreeNode root) {
+    private void print(TreeNode root) {
         if (root == null) {
             return;
         }
@@ -33,69 +43,31 @@ public class ZigZagTreeTraversal {
         Stack<TreeNode> nextLevel = new Stack<>();
         currentLevel.add(root);
         boolean isLeftToRight = true;
-
-
         while (!currentLevel.isEmpty()) {
-            TreeNode node = currentLevel.pop();
-            System.out.print(node.data + " ");
+            TreeNode temp = currentLevel.pop();
+            System.out.print(temp.data + " ");
             if (isLeftToRight) {
-                if (node.left != null) {
-                    nextLevel.add(node.left);
+                if (temp.left != null) {
+                    nextLevel.add(temp.left);
                 }
-                if (node.right != null) {
-                    nextLevel.add(node.right);
+                if (temp.right != null) {
+                    nextLevel.add(temp.right);
                 }
             } else {
-                if (node.right != null) {
-                    nextLevel.add(node.right);
+                if (temp.right != null) {
+                    nextLevel.add(temp.right);
                 }
-                if (node.left != null) {
-                    nextLevel.add(node.left);
+                if (temp.left != null) {
+                    nextLevel.add(temp.left);
                 }
             }
-            if(currentLevel.isEmpty()){
-                isLeftToRight = !isLeftToRight;
-                Stack<TreeNode> temp = new Stack<>();
+            if (currentLevel.isEmpty()) {
                 currentLevel = nextLevel;
-                nextLevel = temp;
+                nextLevel = new Stack<>();
+                isLeftToRight = !isLeftToRight;
             }
-        }
-    }
 
-    private void print(TreeNode root) {
-        if (root == null) {
-            return;
         }
-        print(root.left);
-        System.out.print(root.data + " ");
-        print(root.right);
-    }
 
-    private void insert(int data) {
-        if (root == null) {
-            root = new TreeNode(data);
-            return;
-        }
-        TreeNode lastInsertedNode = lastInsertedNode();
-        if (lastInsertedNode.left == null) {
-            lastInsertedNode.left =new TreeNode(data);
-        } else {
-            lastInsertedNode.right = new TreeNode(data);
-        }
-    }
-
-    private TreeNode lastInsertedNode() {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode temp = q.poll();
-            if (temp.left != null && temp.right != null) {
-                q.add(temp.left);
-                q.add(temp.right);
-            } else {
-                return temp;
-            }
-        }
-        return null;
     }
 }

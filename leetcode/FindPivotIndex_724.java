f53e11a82a7592d67bfd0f8d540dfcdb6a2cb44a
rokibrucse <rokibahmed.ru.cse@gmail.com>
2023-06-18 20:53:45 +0600

    ....

--- leetcode/FindPivotIndex_724.java
+++ leetcode/FindPivotIndex_724.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class FindPivotIndex_724 {
 
 
-    public int pivotIndex(int[] nums) {
+    /*public int pivotIndex(int[] nums) {
         int[] leftSum = new int[nums.length];
         int[] rightSum = new int[nums.length];
                 leftSum[0]  = nums[0];
@@ -24,8 +24,26 @@ public class FindPivotIndex_724 {
                 }
         return -1;
     }
+*/
 
+public static int pivotIndex(int[] nums) {
+        int leftSum = nums[0];  
+        int rightSum = nums[nums.length-1];
+                int i=1;
+                int j=nums.length-2;
+        for(;i<nums.length && j>=0;){
+            if(leftSum==rightSum){
+                return i;
+            }
+            if(leftSum<rightSum){
+                leftSum += nums[i++] ;
+            }else{
+                rightSum += nums[j--];
+            }
+        }
+        return -1;    
+    }
     public static void main(String[] args) {
-
+        System.out.println(pivotIndex(new int[]{1,7,3,6,5,6}));
     }
 }
